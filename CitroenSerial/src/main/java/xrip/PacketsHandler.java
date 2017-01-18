aefb29fd37679d7b71f1647a2be5fa5a3deda8e8
Ilya Maslennikov <xrip@blamper.ru>
2017-01-18 14:30:03 +0300

    Debug and send data to serial

--- CitroenSerial/src/main/java/xrip/PacketsHandler.java
+++ CitroenSerial/src/main/java/xrip/PacketsHandler.java
@@ -1,19 +1,33 @@
 package xrip;
 
+import android.content.Context;
 import android.util.Log;
+import android.widget.Toast;
+
+import com.felhr.serialportexample.MainActivity;
+import com.felhr.serialportexample.UsbService;
 
 import java.util.HashMap;
 import java.util.Map;
+
+import static android.R.id.content;
 
 public class PacketsHandler {
     interface PacketHandler {
         void run(Packet packet);
     }
 
+    class Dummy implements PacketHandler {
+        @Override
+        public void run(Packet packet) {
+            // Write your own handler
+        }
+    }
+
     class RDSTitle implements PacketHandler {
         @Override
         public void run(Packet packet) {
-            Log.d("citroen_log", "RDS Title: "+packet.asString());
+            Log.e("citroen_log", "RDS Title: "+packet.asString());
         }
     }
 
@@ -31,6 +45,8 @@ public class PacketsHandler {
     Map<Integer, PacketHandler> handlers = new HashMap<Integer, PacketHandler>();;
 
     public PacketsHandler() {
+        Log.d("citroen_log", "test");
+        handlers.put(0xFFF, new Dummy());
         handlers.put(0x1A1, new InfoMessage());
         handlers.put(0x2A5, new RDSTitle());
     }
